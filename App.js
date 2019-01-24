923dc5cb0b91baeb8471a6a61fe2286b8eee82d0
trideep <trideep.k@talview.com>
2019-01-24 22:19:17 +0530

    L67S06: single page navigation

--- App.js
+++ App.js
@@ -1,66 +1,27 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { Navigation } from 'react-native-navigation';
+import AuthScreen from './src/screens/Auth/Auth';
 
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceList from "./src/components/PlaceList/PlaceList";
-import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
-import { connect } from 'react-redux';
-import { addPlace, selectPlace, deletePlace, deselectPlace } from './store/actions/index';
+//Register Screens
+Navigation.registerComponent("awesome-place.AuthScreen", () => AuthScreen);
 
-class App extends Component {
-
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName)
-  };
-  onItemSelected = key => {
-    this.props.onSelectPlace(key)
-  };
-  onItemDeleted = () => {
-    this.props.onDeletePlace()
-    this.props.onDeselectPlace()
-  }
-  onModalClose = () => {
-    this.props.onDeselectPlace()
-  }
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail selectedPlace={this.props.selectedPlace}
-          onModalClose={this.onModalClose}
-          onItemDeleted={this.onItemDeleted}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.props.places} onItemSelected={this.onItemSelected}/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
-  }
+//Start App
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [{
+          component: {
+            name: "awesome-place.AuthScreen",
+          }
+        }],
+        options: {
+          topBar: {
+            title: {
+              text: 'Login'
+            }
+          }
+        }
+      }
+    }
+  });
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (name) => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: (key) => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App)
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/** @format */
-
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './store/configureStore';
